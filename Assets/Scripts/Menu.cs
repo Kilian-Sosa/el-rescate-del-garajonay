8ac8989acb65deea49dfe4a0a1139631e57ae609
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2024-02-22 17:45:13 +0000

    feat: Add gamepad support

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,6 +1,19 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour {
+
+    private void Update() {
+        if (SceneManager.GetActiveScene().name == "Menu") {
+            if (Input.GetButtonDown("Jump")) Play();
+            if (Input.GetButtonDown("Fire3")) Credits();
+        }
+        if (SceneManager.GetActiveScene().name == "Victory" && Input.GetButtonDown("Cancel")) OpenMenu();
+        if (SceneManager.GetActiveScene().name == "GameScene") {
+            if (Input.GetButtonDown("Submit")) Unpause();
+            if (Input.GetButtonDown("Cancel")) OpenMenu();
+        }
+    }
 
     public void Play() {
         SCManager.instance.LoadScene("GameScene");
